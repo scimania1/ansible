2527912f2caecaa3f9a4de617c2a9f3122174ce6
scimania <gauravdeepubhi@gmail.com>
2023-08-17 00:27:29 +0530

    hopefully fixed the aur issue

--- install.sh
+++ install.sh
@@ -6,4 +6,12 @@ sudo pacman -S python python-pip python-pipx
 pipx install --include-deps ansible
 ansible-galaxy collection install community.general
 
+CWD=$(pwd)
+mkdir $HOME/src
+cd $HOME/src
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd $CWD
+
 ansible-playbook --ask-become-pass local.yml
